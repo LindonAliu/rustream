129c7975cd7aeb91110fbac212d74b14ba936bd2
LindonAliu <lindon.aliu@outlook.fr>
2024-05-03 22:03:07 +0200

    feat: add parameters handler

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-extern crate m3u;
-use m3u::Reader;
+use rustream::params::{parse_args, Params};
+use rustream::types::Result;
+
+fn run() -> Result<()> {
+    let params: Params = parse_args(std::env::args())?;
+
+    Ok(())
+}
 
 fn main() {
-    let filename = std::env::args().nth(1).unwrap();
-    let mut reader = Reader::open_iptv(filename).unwrap();
-    let chans: Vec<_> = reader.iptv_entries().map(Result::unwrap).collect();
-
-    for chan in chans {
-        println!("{:?}", chan);
+    if let Err(e) = run() {
+        eprintln!("Error: {}", e);
+        std::process::exit(1);
     }
 }
