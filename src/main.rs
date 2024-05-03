caa78b98649db9697bb2f9b2d6f5f3e164adda15
LindonAliu <lindon.aliu@outlook.fr>
2024-05-03 21:40:10 +0200

    feat(m3u): add m3u crate for IPTV support and a first parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate m3u;
+use m3u::Reader;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = std::env::args().nth(1).unwrap();
+    let mut reader = Reader::open_iptv(filename).unwrap();
+    let chans: Vec<_> = reader.iptv_entries().map(Result::unwrap).collect();
+
+    for chan in chans {
+        println!("{:?}", chan);
+    }
 }
